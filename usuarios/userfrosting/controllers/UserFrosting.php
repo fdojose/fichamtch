08fecf0918d1ca62ef5f94e353aab486d06728d1
fdojose <fdojose@yahoo.com>
2015-06-29 22:32:40 -0300

    usuarios funcionando

---
+++ usuarios/userfrosting/controllers/UserFrosting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UserFrosting;
+
+/**
+ *
+ * @property \Fortress\MessageTranslator $translator
+ * @property \Fortress\MessageStream $alerts
+ * @property \UserFrosting\SiteSettingsInterface $site
+ * @property \UserFrosting\UserObjectInterface $user
+ * @property \UserFrosting\PageSchema $schema
+ */
+class UserFrosting extends \Slim\Slim {
+
+
+
+}
